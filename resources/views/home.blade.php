4c5b9bdd163129284e42a16e8e50473deb38af2e
Sekiphp <seki@lostwar.cz>
2018-03-09 15:03:48 +0100

    Priprava sablon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+        <div class="col-md-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading">Přehled projektů</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,7 +14,30 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <table class='table table-bordered table-stripped'>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Název</th>
+                                <th>Datum</th>
+                                <th>Typ</th>
+                                <th>Webový?</th>
+                                <th>Akce</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+
                 </div>
             </div>
         </div>
