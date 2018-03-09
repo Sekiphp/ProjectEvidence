136fa6404adfa88a81209b766a065482fd2e90b3
Sekiphp <seki@lostwar.cz>
2018-03-09 15:10:44 +0100

    Migrace

---
+++ database/migrations/2018_03_09_134007_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 80);
+            $table->date('end_date');
+            $table->integer('project_type')->unsigned();
+            $table->foreign('project_type')->references('id')->on('project_types');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
